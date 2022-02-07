a8ce46515e4c741fb73c5e523fbfed6b274843d5
soblab <soblab@Whit216-17.it.farmingdale.edu>
2022-02-07 10:16:51 -0500

    Added userinfo FXML file with controller

---
+++ src/main/java/com/mycompany/lab_01/UserinfoController.java
@@ -0,0 +1,20 @@
+
+package com.mycompany.lab_01;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+
+public class UserinfoController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
