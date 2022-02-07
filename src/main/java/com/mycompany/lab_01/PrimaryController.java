a260052754ee77263ba194d9ff9a6396fe111cf1
soblab <soblab@Whit216-17.it.farmingdale.edu>
2022-02-07 10:01:51 -0500

    Showed textField message in dialog box

--- src/main/java/com/mycompany/lab_01/PrimaryController.java
+++ src/main/java/com/mycompany/lab_01/PrimaryController.java
@@ -7,18 +7,20 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.TextField;
 
 public class PrimaryController {
-    
+
     @FXML
     private TextField textFieldMessage;
-    
+
     @FXML
-    private void showMessage(){
-        System.out.println("Button Was Pressed");
-        Alert a = new Alert(AlertType.CONFIRMATION);
-        a.setContentText("Show Message Button Pressed");
+    private void showMessage() {
+        //System.out.println("Button Was Pressed");
+        Alert a = new Alert(AlertType.INFORMATION);
+        //a.setContentText("Show Message Button Pressed");
+        String msg = textFieldMessage.getText();
+        a.setContentText(msg);
         a.show();
     }
-    
+
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
