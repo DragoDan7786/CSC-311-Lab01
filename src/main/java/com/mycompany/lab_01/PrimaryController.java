5dd8b621d1ebc00426b07a6225b918da66f67f0d
soblab <soblab@Whit216-17.it.farmingdale.edu>
2022-02-07 09:56:18 -0500

    Got the dialog box to show

--- src/main/java/com/mycompany/lab_01/PrimaryController.java
+++ src/main/java/com/mycompany/lab_01/PrimaryController.java
@@ -2,9 +2,23 @@ package com.mycompany.lab_01;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.TextField;
 
 public class PrimaryController {
-
+    
+    @FXML
+    private TextField textFieldMessage;
+    
+    @FXML
+    private void showMessage(){
+        System.out.println("Button Was Pressed");
+        Alert a = new Alert(AlertType.CONFIRMATION);
+        a.setContentText("Show Message Button Pressed");
+        a.show();
+    }
+    
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
